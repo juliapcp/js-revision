de4947164a5cc71f9e2ba069d24d93f203f345e5
Julia Pontes <47482821+juliapcp@users.noreply.github.com>
2019-02-28 18:01:32 -0300

    Add files via upload

--- lacos.js
+++ lacos.js
@@ -1,1 +1,28 @@
-ss
+//for, while, do while, foreac
+
+txt = "info"
+tam = 10
+caractere = "#"
+
+let txt=prompt('Palavra:');
+let tam=parseInt(prompt('Tamanho:'));
+let caractere=prompt('Caractere:');
+while(txt.length<tam){
+    txt+=caractere;
+}
+console.log(txt);
+
+const nomes=[]; //vetor/array
+console.log(nomes.length); //0
+nomes.push('Bernardo');
+nomes.push('Marina');
+console.log(nomes); 
+console.table(nomes);//tabela
+
+for(let i = 0; i< nomes.length; i++){
+    console.log(`Nome ${i+1}, nomes[i]`);
+}
+//for each (para cada) nao tem índice, cada nome da lista nomes se tornou uma constante
+for (const nome of nomes){
+    console.log('Nome ', nome);
+}
