255250a701cd1c70a0372cf56305f7c349581239
Julia Pontes <47482821+juliapcp@users.noreply.github.com>
2019-02-28 15:41:43 -0300

    Add files via upload

---
+++ instrucoes.js
@@ -0,0 +1,6 @@
+// o que é uma instrução? uma ordem
+// depende da sintaxe da linguagem
+// os ; delimitam o final de uma instrução
+// ex.:
+console.log("imprima isto! ordem 1");
+console.log("ordem 2"); console.log("ordem 3");
--- um.js
+++ um.js
@@ -1,4 +1,11 @@
-var x = 'exemplo bobo';
-if(true){
+var x = 'exemplo bobo'; //var é uma variável global (acessível em qualquer lugar, ruim)
+if(x==2){
     var y = 'outro exemplo bobo';
+}
+if(true){
+    let z='variavel com escopo'; //variavel com escopo, só cria ela se entrar no if
+    if (true){
+        console.log(z); 
+        //estar no mesmo escopo significa estar no mesmo conjunto de chaves, se tiver um if e nesse if for declarado um let, se for criado mais um if dentro desse if vai dar pra ver o valor do let, porque ainda ta no mesmo if externo (conjunto de chaves)
+    }
 }
